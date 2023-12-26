0f701160ab6adde95a8aa381f64066231ef4d4cb
matth <matt.hiessler@gmail.com>
2023-12-26 16:34:53 +0100

    FactList

--- src/FactList.c
+++ src/FactList.c
@@ -3,6 +3,7 @@
 #include "stdio.h"
 #include "string.h"
 
+//Function to test if a Properties is empty (based on selected criteria)
 Bool isEmptyProperty(Properties* prop){
     if(prop->name != NULL){
         return False;
@@ -11,6 +12,7 @@ Bool isEmptyProperty(Properties* prop){
     }
 }
 
+//Function to compare 2 Properties (based on selected criteria) and return True if they are the same and False otherwise
 Bool cmpProperty(Properties* p1, Properties* p2){
     if(strcmp(p1->name,p2->name)==0){
         return True;
@@ -19,12 +21,14 @@ Bool cmpProperty(Properties* p1, Properties* p2){
     }
 }
 
+//Methode to display a Properties
 void printProperties(Properties* p){
     if(p!=NULL){
         printf("name:%s desc:%s\n",p->name,p->desc);
     }
 }
 
+//Function to transform a Properties to a Properties pointer
 Properties* affectField(Properties fact){
     if(fact.desc != NULL && fact.name != NULL){
         Properties* newl = malloc(sizeof(Properties));
@@ -36,10 +40,12 @@ Properties* affectField(Properties fact){
     }
 }
 
+//Function to create an empty FactList
 FactList createFactList(){
     return malloc(sizeof(FactList));
 }
 
+//Function to add a fact(Properties) in tail to a FactList
 FactList addFact(FactList list,Properties fact){
     ElmOfFact* newl = malloc(sizeof(ElmOfFact));
     if((newl->fact= affectField(fact)) == NULL){
@@ -52,6 +58,7 @@ FactList addFact(FactList list,Properties fact){
     return newl;
 }
 
+//Function to free() all element of a FactList
 FactList remouveAllFacts(FactList list){
     ElmOfFact* point = list;
     while (point != NULL){
@@ -62,6 +69,7 @@ FactList remouveAllFacts(FactList list){
     return point;
 }
 
+//Fonction to get the fact in a FactList and NULL if the fact is not in the Factlist
 Properties* isInFactList(FactList list, Properties* fact){
     if(list==NULL){return NULL;}
     if(cmpProperty(list->fact,fact)==True){return list->fact;}
