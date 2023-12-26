ca150c959c03ef0aae98d2ae09fb0c222d25afd6
matth <matt.hiessler@gmail.com>
2023-12-26 16:18:03 +0100

    FactList

--- include/Rules.h
+++ include/Rules.h
@@ -1,18 +1,12 @@
-typedef enum {
-    True, False
-}Bool;
-
-typedef struct {
-    char* name;
-    char* desc;
-}Properties;
+#include "define.h"
+#include "FactList.h"
 
 typedef struct elm1{
-    Properties value;
+    Properties* value;
     struct elm1* next;
-}ElmOfProperties;
+}ElmOfRules;
 
-typedef ElmOfProperties * ListOfProperties;
+typedef ElmOfRules * ListOfProperties;
 
 typedef struct elm2{
     ListOfProperties premise;
@@ -22,12 +16,12 @@ typedef struct elm2{
 
 
 Rules* createEmptyRule();
-Rules* addPremise(Rules* rules,Properties premise);
+Rules* addPremise(Rules* rules,Properties* premise);
 Rules* createConclusion(Rules* rule, Properties* conclusion);
-Bool PropertiesInPremise(ListOfProperties premise, Properties prop);
-Rules* remouvePremise(Rules* rule,Properties premise);
+Bool PropertiesInPremise(ListOfProperties premise, Properties* prop);
+Rules* remouvePremise(Rules* rule,Properties* premise);
 Bool isPremiseEmpty(Rules* rule);
-ElmOfProperties* getHeadOfPremise(Rules* rule);
+ElmOfRules* getHeadOfPremise(Rules* rule);
 Properties* getConclusion(Rules* rule);
 void printPremise(Rules* rule);
 void printRule(Rules* rule);
